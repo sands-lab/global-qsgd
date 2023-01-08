69af27148b001d7cf1eac6e01c09d216f99dacd0
Ubuntu <ubuntu@mcnode22.maas>
2023-01-08 20:09:41 +0000

    Scale by log

--- gqsgd/allreduce.py
+++ gqsgd/allreduce.py
@@ -66,3 +66,44 @@ def exponential_dithering_allreduce(tensor):
             gqsgd_cuda.exponential_dithering_reduce(accum[:], tmp)
         send_req.wait()
     tensor[:] = accum[:]
+
+
+""" Implementation of a tree-reduce with customized add """
+def exponential_dithering_tree_allreduce(tensor):
+    rank = dist.get_rank()
+    size = dist.get_world_size()
+    send_buff = tensor.clone()
+    recv_buff = tensor.clone()
+    layers = int(math.log(size, 2))
+    # Recuesive Reduce
+    for i in range(layers): # 0, 1
+        if rank % (2 ** (i + 1)) == 0:
+            dist.send(send_buff, rank + (2 ** i))
+            tmp=recv_buff[:].clone()
+            gqsgd_cuda.exponential_dithering_reduce(send_buff[:], tmp)
+    tensor[:] = send_buff[:]
+    # Broadcast
+
+    for i in range(size - 1):
+        if i % 2 == 0:
+            # Send send_buff
+            if rank == 0:
+                send_req = dist.isend(send_buff, right)
+                dist.recv(recv_buff, left)
+            else:
+                dist.recv(recv_buff, left)
+                send_req = dist.isend(send_buff, right)
+            tmp=recv_buff[:].clone()
+            gqsgd_cuda.exponential_dithering_reduce(accum[:], tmp)
+        else:
+            # Send recv_buff
+            if rank == 0:
+                send_req = dist.isend(recv_buff, right)
+                dist.recv(send_buff, left)
+            else:
+                dist.recv(send_buff, left)
+                send_req = dist.isend(recv_buff, right)
+            tmp=send_buff[:].clone()
+            gqsgd_cuda.exponential_dithering_reduce(accum[:], tmp)
+        send_req.wait()
+    tensor[:] = accum[:]
