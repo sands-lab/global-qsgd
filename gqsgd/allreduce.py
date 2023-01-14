2700072dd86594723bde011dcddde1a5640ccf32
Ubuntu <ubuntu@mcnode22.maas>
2023-01-14 18:43:44 +0000

    merge to ring allreduce

--- gqsgd/allreduce.py
+++ gqsgd/allreduce.py
@@ -2,40 +2,9 @@ import torch
 import torch.distributed as dist
 import gqsgd_cuda
 import math
-""" Implementation of a ring-reduce with avg """
-def standard_dithering_allreduce(tensor):
-    rank = dist.get_rank()
-    size = dist.get_world_size()
-    send_buff = tensor.clone()
-    recv_buff = tensor.clone()
-    accum = tensor.clone()
-    left = ((rank - 1) + size) % size
-    right = (rank + 1) % size
-    # Ring AllReduce
-    for i in range(size - 1):
-        if i % 2 == 0:
-            # Send send_buff
-            if rank == 0:
-                send_req = dist.isend(send_buff, right)
-                dist.recv(recv_buff, left)
-            else:
-                dist.recv(recv_buff, left)
-                send_req = dist.isend(send_buff, right)
-            accum[:] += recv_buff[:]
-        else:
-            # Send recv_buff
-            if rank == 0:
-                send_req = dist.isend(recv_buff, right)
-                dist.recv(send_buff, left)
-            else:
-                dist.recv(send_buff, left)
-                send_req = dist.isend(recv_buff, right)
-            accum[:] += send_buff[:]
-        send_req.wait()
-    tensor[:] = accum[:]
 
 """ Implementation of a ring-reduce with customized add """
-def exponential_dithering_allreduce(tensor):
+def ring_allreduce(tensor, exponential):
     rank = dist.get_rank()
     size = dist.get_world_size()
     send_buff = tensor.clone()
@@ -54,7 +23,11 @@ def exponential_dithering_allreduce(tensor):
                 dist.recv(recv_buff, left)
                 send_req = dist.isend(send_buff, right)
             tmp=recv_buff[:].clone()
-            gqsgd_cuda.exponential_dithering_reduce(accum[:], tmp)
+            if exponential == True:
+                gqsgd_cuda.exponential_dithering_reduce(accum[:], tmp)
+            else:
+                accum[:] += tmp
+
         else:
             # Send recv_buff
             if rank == 0:
@@ -64,7 +37,10 @@ def exponential_dithering_allreduce(tensor):
                 dist.recv(send_buff, left)
                 send_req = dist.isend(recv_buff, right)
             tmp=send_buff[:].clone()
-            gqsgd_cuda.exponential_dithering_reduce(accum[:], tmp)
+            if exponential == True:
+                gqsgd_cuda.exponential_dithering_reduce(accum[:], tmp)
+            else:
+                accum[:] += tmp
         send_req.wait()
     tensor[:] = accum[:]
 
