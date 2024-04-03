848034926448b44bf8b9f5ad8195a09012e94889
JihaoXin <jihaoxin1998@gmail.com>
2024-04-03 12:09:34 +0000

    ResNet resize

--- models/ResNet/launch.sh
+++ models/ResNet/launch.sh
@@ -1,21 +1,11 @@
 seeds=(666 999)
+DATA_DIR=/mnt/xinj/miniimagenet
 for seed in ${seeds[@]}
 do
-python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook default /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/default_seed$seed.txt 2>&1
-python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook qsgd /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/qsgd_seed$seed.txt 2>&1
-python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook standard_dithering /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/standard_seed$seed.txt 2>&1
-python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook exponential_dithering /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/exponential_seed$seed.txt 2>&1
-python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook powersgd /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/powersgd_seed$seed.txt 2>&1
-python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook lgreco /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/lgreco_seed$seed.txt 2>&1
+python3 main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook default $DATA_DIR >logs/seed$seed/default_seed$seed.txt 2>&1
+python3 main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook qsgd $DATA_DIR >logs/seed$seed/qsgd_seed$seed.txt 2>&1
+python3 main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook standard_dithering $DATA_DIR >logs/seed$seed/standard_seed$seed.txt 2>&1
+python3 main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook exponential_dithering $DATA_DIR >logs/seed$seed/exponential_seed$seed.txt 2>&1
+python3 main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook powersgd $DATA_DIR >logs/seed$seed/powersgd_seed$seed.txt 2>&1
+python3 main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook lgreco $DATA_DIR >logs/seed$seed/lgreco_seed$seed.txt 2>&1
 done
-
-# seeds=(666 999)
-# for seed in ${seeds[@]}
-# do
-# python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook default /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/default_seed$seed.txt 2>&1
-# python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook qsgd /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/qsgd_seed$seed.txt 2>&1
-# python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook standard_dithering /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/standard_seed$seed.txt 2>&1
-# python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook exponential_dithering /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/exponential_seed$seed.txt 2>&1
-# python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook powersgd /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/powersgd_seed$seed.txt 2>&1
-# python main.py -a wide_resnet101_2 --epochs=90 --batch-size 64 --dist-url 'tcp://127.0.0.1:12701' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --seed=$seed --hook lgreco /home/ubuntu/jihao/data/mini-imagenet >logs/seed$seed/lgreco_seed$seed.txt 2>&1
-# done
