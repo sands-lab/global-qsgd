dc78e6ae64ad4b7f14a2fce9368e566eeddfb8af
JihaoXin <46529913+JihaoXin@users.noreply.github.com>
2025-07-24 21:09:52 +0300

    Update TransformerXL

--- models/TransformerXL/pytorch/launch.sh
+++ models/TransformerXL/pytorch/launch.sh
@@ -6,10 +6,8 @@ workers=4
 rank=32
 for seed in 666
 do
-    # for method in "default" "qsgd" "standard_dithering" "exponential_dithering" "powersgd" "lgreco" "standard_dithering_4bit"
-    for method in "standard_dithering_4bit"
+    for method in "default" "qsgd" "standard_dithering" "exponential_dithering" "powersgd" "lgreco" "standard_dithering_4bit"
     do
-        # echo "NCCL_P2P_DISABLE: $NCCL_P2P_DISABLE" > logs/${method}_${seed}.txt
         bash run_wt103_base.sh train $workers $rank TF32/DP32_$seed $seed $method --config dgxa100_4gpu_tf32 >> logs/${method}_${seed}.txt 2>&1
     done
 done
